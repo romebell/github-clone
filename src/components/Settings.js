3e25f706a0fceff8055052427a023ea4d1e15966
jwright1992 <jordanwright1992@gmail.com>
2021-06-01 13:42:29 -0700

    Initial Commit

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 
 const Settings = (props) => {
+    return (
+        <div>
+          <h1>Dentist Website</h1>
+          <p>
+            Welcome to my dentist website.
+          </p>
+        </div>
+      )
 
 }
 
