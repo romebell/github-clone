22b7120508df719030d5bf1f71c65340cda8a0da
jakeschultz89 <ejacobschultz@gmail.com>
2021-06-01 16:42:01 -0400

    works but could not have done this alone. this is MAJOR help from Omar and the other guys

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Dashboard = (props) => {
-
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {props.apiRes.login}
+        </div>
+    )
 }
 
-export default Dashboard;
+export default Dashboard
--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,7 +1,21 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 const Repositories = (props) => {
 
+    const repoList = props.repos.map((item, idx) => {
+        return <li key={idx}>{item.name}</li>
+    })
+    console.log(props)
+    return (
+        <div>
+            <h1>Repositories</h1>
+            <ul>
+                {repoList}
+            </ul>
+        </div>
+    )
 }
 
-export default Repositories;
+export default Repositories
