6736c87eb77fe01fea3f0ae252a8167b31138fe2
vinnyvilasboa <vinnycesca@gmail.com>
2021-06-01 16:49:06 -0400

    location into repos component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import './App.css';
+import Axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import Settings from './components/Settings'
+import Explore from './components/Explore'
+import Repositories from './components/Repositories'
+import Dashboard from './components/Dashboard'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 function App() {
+
+const [apiRes, setApiRes] = useState([]);
+const [repos, setRepos] = useState([]);
+
+async function fetchData(){
+  const request = 
+  await Axios.get('https://api.github.com/users/vinnyvilasboa')
+  const results =
+  await request.data
+  setApiRes(results)
+}
+
+async function fetchRepos(){
+  const reposReq = 
+  await Axios.get('https://api.github.com/users/vinnyvilasboa/repos')
+  const reposRes =
+  await reposReq.data
+  setRepos(reposRes)
+}
+
+
+useEffect (()=> {
+fetchData();
+}, []);
+
+useEffect (()=> {
+  fetchRepos();
+  }, [apiRes]);
+
+// console.log(apiRes);
+
   return (
     <div className="App">
-      <h1>Github Clone</h1>
-      <img width="350px" height="200px" src="https://img.flaticon.com/icons/png/512/25/25231.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="" />
+     
+      {/* <h1>Github Clone</h1> */}
+      <Router>
+        <nav>
+          <Link to="/"><button>DashBoard</button></Link>{" "}
+          <Link to="/explore"><button>Explore</button></Link>{" "}
+          <Link to="/repositories"><button>Repositories</button></Link>{" "}
+          <Link to="/settings"><button>Settings</button></Link>{" "}
+        </nav>
+        <Route exact path="/">
+          <Dashboard apiRes={apiRes}/>
+        </Route>
+        <Route path="/explore">
+          <Explore apiRes={apiRes}/>
+        </Route>
+        <Route path="/repositories" render={({location}) => 
+        <Repositories repos={repos}/>
+      }>
+        </Route>
+        <Route path="/settings">
+          <Settings apiRes={apiRes}/>
+        </Route>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Repositories.js
+++ src/components/Repositories.js
@@ -1,7 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import {  Link } from 'react-router-dom'
 
 const Repositories = (props) => {
+console.log(props)
 
+    const listRepos = props.repos.map((item,index) =>{
+        return <li><Link 
+        to={{
+        pathname:"/repositories/show",
+        state:item}}
+        key={index}>{item.name}
+        </Link></li>
+    })
+    return(
+
+        <div>
+            
+            <h1>REPOS</h1>
+            <ul>
+                {listRepos}
+            </ul>
+        </div>
+    )
 }
 
 export default Repositories;
