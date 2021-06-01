167cc22a13aa86a7413164028e59329af9e6950c
Daniel Lafontant <ldanieljajr@gmail.com>
2021-06-01 12:48:39 -0400

    first commit

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -2,6 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 const Explore = (props) => {
 
+    return (
+        <div>
+            
+        </div>
+    )
+
 }
 
 export default Explore;
