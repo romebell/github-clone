d42e8f4d3b41e2aec7b666b4a44aee57ff5d7074
Eddyfu45 <eddyfu45@yahoo.com>
2021-06-01 17:11:48 -0400

    Repository Display Works

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
 import './App.css';
+import React, {useEffect, useState} from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import Dashboard from './components/Dashboard';
+import Repositories from './components/Repositories';
+import Explore from './components/Explore';
+import Settings from './components/Settings';
 
 function App() {
+  const [apiRes, setApiRes] = useState([])
+  const [repoRes, setRepoRes] = useState([])
+
+  async function fetchData() {
+    const request = await axios.get("https://api.github.com/users/eddyfu45")
+    const results = await request.data
+    setApiRes(results)
+  }
+
+  async function fetchRepo() {
+    if(apiRes.repos_url) {
+      const repoRequest = await axios.get(apiRes.repos_url)
+      const repoResults = await repoRequest.data
+      setRepoRes(repoResults)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+    
+  }, [])
+  
+  useEffect(() => {
+    fetchRepo()
+  }, [apiRes])
+    console.log(apiRes)
+    console.log(repoRes)
+
   return (
     <div className="App">
-      <h1>Github Clone</h1>
-      <img width="350px" height="200px" src="https://img.flaticon.com/icons/png/512/25/25231.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="" />
+      <Router>
+        <nav>
+          <Link to='/'>Dashboard</Link>{" "}
+          <Link to='/explore'>Explore</Link>{" "}
+          <Link to='/repositories'>Repositories</Link>{" "}
+          <Link to='/settings'>Settings</Link>{" "}
+        </nav>
+        <Route exact path='/'>
+          <Dashboard apiRes={apiRes}/>
+        </Route>
+        <Route exact path='/explore'>
+          <Explore apiRes={apiRes}/>
+        </Route>
+        <Route exact path='/repositories'>
+          <Repositories apiRes={repoRes}/>
+        </Route>
+        <Route exact path='/settings'>
+          <Settings apiRes={apiRes}/>
+        </Route>
+      </Router>
     </div>
   );
 }
